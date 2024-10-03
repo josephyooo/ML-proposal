0d5a083aebcafb0d1511ac096e151c2f175ca142
Daniel Pan <56142819+denialpan@users.noreply.github.com>
2024-10-03 18:11:17 +0000

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("ML Proposal")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Hello this is a paragraph."
 )
+
+st.write(
+    "This is another one."
+)
+
+st.image("images/dice.png", caption="some dice this is a caption")
+st.write("seems pretty alright actually")
